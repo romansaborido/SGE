85448b6782181ddba694699bb7dc2ef44c486f3f
romansaborido <roman.saborido@iesnervion.es>
2025-10-01 14:42:07 +0200

    initial commit

---
+++ Hola_Mundo/Hola_Mundo/Default.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Hola_Mundo._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <main>
+        <p>
+            <asp:Label ID="lblSaludo" runat="server" Text=""></asp:Label>
+            <asp:TextBox ID="txtNombre" runat="server"></asp:TextBox>
+        </p>
+    </main>
+
+</asp:Content>
---
+++ Hola_Mundo/Hola_Mundo/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Hola_Mundo
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            lblSaludo.Text = "Hola " + nombre;
+        }
+
+    }
+}
